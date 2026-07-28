ebc31680ebe4befa99be2bbdc21bf5630b17f265
Adelodunpeter25 <adelodunpeter69@gmail.com>
2026-07-28 13:50:08 +0100

    fix: resolve sidebar import clash on case-insensitive filesystem

--- apps/desktop/src/pages/ChatPage.tsx
+++ apps/desktop/src/pages/ChatPage.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Settings as SettingsIcon } from "lucide-react";
 import { useNavigate } from "@tanstack/react-router";
 import { TitleBar } from "../components/TitleBar";
-import { Sidebar } from "../components/Sidebar";
+import { Sidebar } from "../components/sidebar";
 import { ChatScreen } from "../screens/ChatScreen";
 import { EmptyState } from "../components/common";
 import { CommandPalette } from "../components/commandpalette";
